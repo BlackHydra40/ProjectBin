a32feb8dbdaf6beba8858d138e34407adb72ad61
BLACK_HYDRA <112259891+BlackHydra40@users.noreply.github.com>
2022-12-21 08:27:04 -0300

    Add files via upload

---
+++ pb.py
@@ -0,0 +1,109 @@
+import random
+import os
+import sys
+import time
+##By: MS40
+##ProjectBIN
+#CORES
+rojo = '\033[31;1m'
+azul = '\033[34;1m'
+verde = '\033[32;1m'
+amarillo = '\033[33;1m'
+morado = '\033[35;1m'
+celeste = '\033[36;1m'
+plomo = '\033[30;1m'
+close = '\033[0m'
+os.system("clear")
+bi = str(input('Você deseja instalar os Requisitos ? s ou n ?: '))
+if bi == 's':
+   os.system("bash install.sh")
+else:
+
+   os.system("clear")
+os.system("cat baner.txt | lolcat")
+print(celeste)
+print('FEITO PELO MS40!!!')
+b = True
+c = False
+
+s = True
+n = False
+
+bin = random.randrange(333333, 555555)
+bin1 = random.randrange(333333, 555555)
+bin2 = random.randrange(333333, 555555)
+bin3 = random.randrange(333333, 555555)
+bin4 = random.randrange(333333, 555555)
+bin5 = random.randrange(333333, 555555)
+bin6 = random.randrange(333333, 555555)
+bin7 = random.randrange(333333, 555555)
+bin8 = random.randrange(333333, 555555)
+bin9 = random.randrange(333333, 555555)
+bin10 = random.randrange(333333, 555555)
+
+#////////////////////////////////////////////////
+
+cpf = random.randrange(12345678900, 99999999999)
+cpf1 = random.randrange(12345678900, 99999999999)
+cpf2 = random.randrange(12345678900, 99999999999)
+cpf3 = random.randrange(12345678900, 99999999999)
+cpf4 = random.randrange(12345678900, 99999999999)
+cpf5 = random.randrange(12345678900, 99999999999)
+cpf6 = random.randrange(12345678900, 99999999999)
+cpf7 = random.randrange(12345678900, 99999999999)
+cpf8 = random.randrange(12345678900, 99999999999)
+cpf9 = random.randrange(12345678900, 99999999999)
+cpf10 = random.randrange(12345678900, 99999999999)
+
+bi = str(input('Você deseja gerar uma BIN ou um cpf ? b=BIN ou c=CPF?: '))
+
+if bi == 'b':
+    os.system("clear")
+    os.system ("toilet -f big 'gerando' -F gay | lolcat")
+    time.sleep(1)
+    print(amarillo)
+    print('BIN GERADA :', bin)
+    print('BIN GERADA :', bin1)
+    print('BIN GERADA :', bin2)
+    print('BIN GERADA :', bin3)
+    print('BIN GERADA :', bin4)
+    print('BIN GERADA :', bin5)
+    print('BIN GERADA :', bin6)
+    print('BIN GERADA :', bin7)
+    print('BIN GERADA :', bin8)
+    print('BIN GERADA :', bin9)
+    print('BIN GERADA :', bin10)
+else:
+    os.system("clear")
+    os.system ("toilet -f big 'gerando' -F gay | lolcat")
+    time.sleep(1)
+    print(amarillo)
+    print('CPF GERADA :', cpf)
+    print('CPF GERADA :', cpf1)
+    print('CPF GERADA :', cpf2)
+    print('CPF GERADA :', cpf3)
+    print('CPF GERADA :', cpf4)
+    print('CPF GERADA :', cpf5)
+    print('CPF GERADA :', cpf6)
+    print('CPF GERADA :', cpf7)
+    print('CPF GERADA :', cpf8)
+    print('CPF GERADA :', cpf9)
+    print('CPF GERADA :', cpf10)
+    bi = str(input('deseja ver se o cpf é valida no nosso grupo de puxa dados ? s ou n?: '))
+if bi == 's':
+    print('ENTRANDO NO TELEGRAM')
+    os.system("xdg-open https://t.me/BlackCHKNGKS &> /dev/null && sleep 10")
+    os.system("python3 pb.py")
+else:
+    bi = str(input('deseja deseja continuar no ProjectBIN ? s ou n?: '))
+    os.system("python3 pb.py")
+
+#///////////////////////////////////////////////////////////////////////////////////////////////
+
+if bi == 's':
+    print('ENTRANDO NO TELEGRAM')
+    os.system("xdg-open https://t.me/BlackCHKNGKS &> /dev/null && sleep 10")
+    os.system("python3 pb.py")
+else:
+    bi = str(input('deseja continuar no ProjectBIN ? s ou n?: '))
+    os.system("python3 pb.py")
